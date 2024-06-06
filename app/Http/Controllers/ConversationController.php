83dc23bb4b2e3306fb41ac374b78e6700905b8e0
Kertsu <kurtddbigtas@gmail.com>
2024-06-06 16:51:24 +0800

    add ss pagination: get conversations

--- app/Http/Controllers/ConversationController.php
+++ app/Http/Controllers/ConversationController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Events\ReadMessage;
 use App\Models\Conversation;
+use App\Models\Message;
 use App\Traits\HttpResponsesTrait;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
@@ -11,18 +12,39 @@ use Illuminate\Support\Facades\Auth;
 class ConversationController extends Controller
 {
     use HttpResponsesTrait;
-    public function index()
+    public function index(Request $request)
     {
         $user = Auth::user();
-        $conversations = Conversation::where('initiator_id', $user->id)
+
+        $query = Conversation::where('initiator_id', $user->id)
             ->orWhere('recipient_id', $user->id)
-            ->with(['latestMessage', 'recipient'])->orderBy('updated_at', 'desc')->get();
+            ->with(['latestMessage', 'recipient'])
+            ->orderByDesc(
+                Message::select('created_at')
+                    ->whereColumn('conversation_id', 'conversations.id')
+                    ->latest()
+                    ->limit(1)
+            );
+            
+
+        if ($request->has('first') && $request->has('page') && $request->has('rows')) {
+            $page = $request->input('page');
+            $rows = $request->input('rows');
+
+            $offset = ($page - 1) * $rows;
+
+            $query->skip($offset)->take($rows);
+        }
+
+        $conversations = $query->get();
 
         return $this->success([
             'conversations' => $conversations,
-            'totalRecords' => $conversations->count(),
+            'totalRecords' => $conversations->count(), 
         ]);
     }
+
+
 
     public function get_messages(Conversation $conversation)
     {
